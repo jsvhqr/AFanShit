a24f7b3806df0857b21dc7edc706c59a7fa472c1
Johan Svedlund Nordström <jsvhqr@yahoo.com>
2016-04-09 21:10:37 +0200

    LUL

--- src/webapp/node/WebServer.js
+++ src/webapp/node/WebServer.js
@@ -1,9 +1,23 @@
-// set up ========================
-  var express  = require('express');
-  var app      = express();
+var express = require('express');
+var app = express();
+var path = require('path');
 
-  app.use(express.static('/home/jsvhqr/DevWeb/AFanShit/src/webapp'));// set the static files location /public/img will be /img for users
 
-  // listen (start app with node server.js) ======================================
-  app.listen(8080);
-  console.log("App listening on port 8080");
+app.get("/", function (req, res) {
+    var ip = req.headers['x-forwarded-for'] ||
+        req.connection.remoteAddress ||
+        req.socket.remoteAddress ||
+        req.connection.socket.remoteAddress;
+    console.log("Request from " + ip);
+    res.sendfile(path.resolve('../index.html'));
+});
+
+/* serves all the static files */
+app.get(/^(.+)$/, function (req, res) {
+    console.log('static file request : ' + req.params);
+    res.sendfile(path.resolve('..') + req.params[0]);
+});
+
+app.listen(8080);
+
+console.log("App listening on port 8080");
