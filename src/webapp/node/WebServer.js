e6947bf035c310c3be10644c8e50ba5c746bf74b
Johan Svedlund Nordström <jsvhqr@yahoo.com>
2016-04-09 21:17:30 +0200

    LUL

--- src/webapp/node/WebServer.js
+++ src/webapp/node/WebServer.js
@@ -1,6 +1,11 @@
 var express = require('express');
 var app = express();
 var path = require('path');
+var steamkey = "1ED27D8168C151D5FF743AB7FAC2151E";
+var steamBaseUri = "https://api.steampowered.com/IDOTA2Match_570/";
+
+
+
 
 
 app.get("/", function (req, res) {
@@ -17,6 +22,26 @@ app.get(/^(.+)$/, function (req, res) {
     console.log('static file request : ' + req.params);
     res.sendfile(path.resolve('..') + req.params[0]);
 });
+
+app.get("/matchHistory/:member", function (req, res) {
+    console.log('request for : ' + req.getParameter("member") + " matchhistory" );
+    var memberreq = req.getParameter("member");
+
+    if(memberreq === 'Loda'){
+
+    }else if(memberreq === 's4'){
+
+    }else if(memberreq === 'AdmiralBulldog'){
+
+    }else if(memberreq === 'Akke'){
+
+    }else if(memberreq === 'EGM'){
+
+    }
+});
+
+
+
 
 app.listen(8080);
 
