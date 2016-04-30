629a2046cf0f7cfca4a163c738a83099cb74a0a1
jsvhqr <jsvhqr@yahoo.com>
2016-04-30 23:08:59 +0200

    LUL

--- src/webapp/node/functions/utilFunctions.js
+++ src/webapp/node/functions/utilFunctions.js
@@ -1,39 +1,67 @@
-/**
- * Created by jsvhqr on 2016-04-23.
- */
+    /**
+     * Created by jsvhqr on 2016-04-23.
+     */
 
-getHeroPlayed = function(id,hs) {
-    for(var i = 0; i < hs.length; i++){
-        if(hs[i].id === id){
-            return hs[i].name;
+    getHeroPlayed = function(id,hs) {
+        for(var i = 0; i < hs.length; i++){
+            if(hs[i].id === id){
+                return hs[i].name;
+            }
         }
-    }
-    return "unknown";
-}
-
-isMember  = function(memberKey,account_id){
-    return memberKey == account_id;
-}
-
-
-item = function (itemID, items) {
-    
-    for(var i = 0;i<items.length;i++){
-        if(itemID == items[i].id){
-            return items[i].name;
-        }
+        return "unknown";
     }
 
-}
+    isMember  = function(memberKey,account_id){
+        return memberKey == account_id;
+    }
 
-isDire = function(a,b){
-    return a == b;
-}
 
-module.exports.getHeroPlayed = getHeroPlayed;
+    item = function (itemID, items) {
 
-module.exports.isMember = isMember;
+        for(var i = 0;i<items.length;i++){
+            if(itemID == items[i].id){
+                return items[i].name;
+            }
+        }
 
-module.exports.item = item;
+    }
 
-module.exports.isDire = isDire;
+    isDire = function(a,b){
+        return a == b;
+    }
+
+    getMatchType = function (lobby_type) {
+
+        switch (lobby_type) {
+            case 0 :
+                return "Public matchmaking";
+            case 1 :
+                return "Practise";
+            case 2 :
+                return  "Tournament game";
+            case 3 :
+                return  "Tutorial ";
+            case 4 :
+                return  "Bot game";
+            case 5 :
+                return  "Team match";
+            case 6 :
+                return  "Solo queue";
+            case 7 :
+                return  "Ranked matchmaking";
+            default:
+                return "1v1 Solo mid";
+
+        }
+
+    }
+
+    module.exports.getHeroPlayed = getHeroPlayed;
+
+    module.exports.isMember = isMember;
+
+    module.exports.item = item;
+
+    module.exports.isDire = isDire;
+
+    module.exports.getMatchType = getMatchType;
